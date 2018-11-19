d8b782a783ba6cb45fda504ebd251bb22c93a3d0
Andreas Wolf <flowpower@t-online.de>
2018-11-19 17:20:10 +0100

    draft version

---
+++ schachgrossmeister/src/schachgrossmeister/chessData.java
@@ -0,0 +1,10 @@
+package schachgrossmeister;
+
+public class chessData {
+	int data[] = {0,1,2};
+	
+	public void display() {
+		System.out.println("implement data display");
+	}
+
+}
---
+++ schachgrossmeister/src/schachgrossmeister/github.java
@@ -0,0 +1,19 @@
+package schachgrossmeister;
+
+public class github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		chessData data = new chessData();
+		
+		schachgrossmeister();   
+		data.display();
+		
+		
+
+	}
+	public static void schachgrossmeister() {
+		System.out.println("schachgrossmeister");
+	}
+
+}
